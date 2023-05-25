03298983986af332d27df58a01148bf15bcaf41b
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-05-25 17:02:45 +0900

    :sparkles: news Flow

--- Targets/MyApp/Sources/Application/Flow/Items/NewsFlow.swift
+++ Targets/MyApp/Sources/Application/Flow/Items/NewsFlow.swift
@@ -1,9 +1,47 @@
-//
-//  NewsFlow.swift
-//  KickOff
-//
-//  Created by 임준화 on 2023/05/25.
-//  Copyright © 2023 com.KickOff. All rights reserved.
-//
 
-import Foundation
+import RxFlow
+import RxRelay
+import UIKit
+
+struct NewsStepper: Stepper{
+    let steps: PublishRelay<Step> = .init()
+    
+    var initialStep: Step{
+        return KOStep.newsIsRequired
+    }
+}
+
+final class NewsFlow: Flow{
+    // MARK: - Properties
+    var root: Presentable{
+        return self.rootVC
+    }
+    
+    @Inject private var vc: NewsVC
+    @Inject var stepper: NewsStepper
+    private let rootVC = UINavigationController()
+    
+    // MARK: - Init
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    
+    // MARK: - Navigate
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step.asKOStep else { return .none }
+        switch step{
+        case .newsIsRequired:
+            return coordinateToNews()
+        default:
+            return .none
+        }
+    }
+}
+
+// MARK: - Method
+private extension NewsFlow{
+    func coordinateToNews() -> FlowContributors {
+        self.rootVC.setViewControllers([vc], animated: true)
+        return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vc.reactor ?? .init()))
+    }
+}

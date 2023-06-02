7edc9ba716982ddf3d708fa799c1e2058fb58f76
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-06-02 15:25:29 +0900

    :seedling: UIView addsubviews extension

--- Targets/MyApp/Sources/Presentation/Scene/TabItems/League/LeagueVC.swift
+++ Targets/MyApp/Sources/Presentation/Scene/TabItems/League/LeagueVC.swift
@@ -15,13 +15,26 @@ final class LeagueVC: BaseVC<LeagueViewModel> {
         $0.backgroundColor = .white
     }
     
+    private let segmentedControl = UISegmentedControl(items: ["league1View", "league2View"]).then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.addTarget(self, action: #selector(switchView), for: .touchUpInside)
+    }
+    
+    private let league1View = UIView().then{
+        $0.backgroundColor = .red
+    }
+    
+    private let league2View = UIView().then{
+        $0.backgroundColor = .blue
+    }
+    
     override func configureNavigation() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = "리그"
     }
     
     override func addView() {
-        view.addSubview(leagueListTableView)
+        view.addSubViews(leagueListTableView,segmentedControl, league1View, league2View)
     }
     
     override func setLayout() {
@@ -29,6 +42,21 @@ final class LeagueVC: BaseVC<LeagueViewModel> {
             make.top.equalTo(100)
             make.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
+        }
+        
+        segmentedControl.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(150)
+        }
+    }
+    
+    @objc func switchView(_ sender: UISegmentedControl){
+        if sender.selectedSegmentIndex == 0 {
+            league1View.alpha = 1
+            league2View.alpha = 0
+        } else{
+            league1View.alpha = 0
+            league2View.alpha = 1
         }
     }
 }
--- Targets/MyApp/Sources/Presentation/Scene/TabItems/League/LeagueViewModel.swift
+++ Targets/MyApp/Sources/Presentation/Scene/TabItems/League/LeagueViewModel.swift
@@ -11,4 +11,5 @@ import RxFlow
 
 final class LeagueViewModel: BaseViewModel, Stepper{
     
+    
 }

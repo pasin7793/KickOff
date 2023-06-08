9e2b9b4851deec65dd4f6bc6c6aac0443f6c3e86
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-06-08 17:55:16 +0900

    :art: file 구조 수정

---
+++ Targets/MyApp/Sources/Application/Domain/Entities/League.swift
@@ -0,0 +1,9 @@
+//
+//  League.swift
+//  KickOff
+//
+//  Created by 임준화 on 2023/06/08.
+//  Copyright © 2023 KickOff. All rights reserved.
+//
+
+import Foundation
---
+++ Targets/MyApp/Sources/Presentation/Scene/TabItems/League/Cell/League1Cell.swift
@@ -0,0 +1,9 @@
+//
+//  League1Cell.swift
+//  KickOff
+//
+//  Created by 임준화 on 2023/06/08.
+//  Copyright © 2023 KickOff. All rights reserved.
+//
+
+import Foundation
